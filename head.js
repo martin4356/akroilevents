0115728d245631bfc9ee35b3ba052cf582a03d99
martin4356 <martin4356@gmail.com>
2026-09-18 14:49:29 +0530

    Files
    Only files

---
+++ head.js
@@ -0,0 +1,9 @@
+const script = document.createElement('script');
+script.type = 'application/ld+json';
+script.textContent = `{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "Akroil International Pvt Ltd",
+  "url": "https://akroilevents.com"
+}`;
+document.head.appendChild(script);
